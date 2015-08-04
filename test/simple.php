85c482f3953c5e3976c4aedc5e65782a9e35d113
顾伟刚 <cnwggu@gmail.com>
2015-08-04 17:22:04 +0800

    modify simple test

--- test/simple.php
+++ test/simple.php
@@ -2,13 +2,14 @@
 // binlog_set_position(1751, "MacPro-bin.000005");
 $binlog_handler = binlog_connect("mysql://root@127.0.0.1:3306");
 while($msg = binlog_wait_for_next_event($binlog_handler)) {
-    var_dump($msg);
+    // var_dump($msg);
     // var_dump(binlog_get_position($binlog_handler, $filename));
     // var_dump($filename);
     if(isset($msg["message"])) {
-    $a = trim($msg["message"]);
-    $m = json_decode($a, true);
-    var_dump($m);
-
+        $a = trim($msg["message"]);
+        echo $a . PHP_EOL;
+        $m = json_decode($a, true);
+        echo json_last_error_msg();
+        var_dump($m);
     }
 }
